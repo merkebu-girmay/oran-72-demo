c1154f7719feb2986948d29fc770deddca595d65
Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>
2026-02-13 20:21:30 +0530

    Add Positioning Information Exchange RRC + MAC procedures: Direct
    Signed-off-by: Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>

--- openair2/RRC/NR/rrc_gNB_NRPPA.h
+++ openair2/RRC/NR/rrc_gNB_NRPPA.h
@@ -7,3 +7,5 @@
 
 int rrc_gNB_process_trp_information_request(gNB_RRC_INST *rrc, const nrppa_trp_information_req_t *msg);
 int rrc_CU_process_trp_information_response(f1ap_trp_information_resp_t *f1ap_msg);
+void rrc_gNB_process_positioning_information_request(gNB_RRC_INST *rrc, const nrppa_positioning_information_req_t *msg);
+void rrc_CU_process_positioning_information_response(f1ap_positioning_information_resp_t *f1ap_msg);
